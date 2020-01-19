2952caaf1d16b04f08708f38af31cc8af050c15d
Ben Mulholland <ben.mulholland@gmail.com>
2020-01-19 06:50:17 -0500

    linking to js file, button div started, images shown, need to get background to show and get onclick function to work next

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+$buttons()
+
+
+
+
+  var targetNumber = 53;  //this will be a random number 
+
+  $("#number-to-guess").text(targetNumber);
+
+  var counter = 0;
+
+  // Now for the hard part. Creating multiple crystals each with their own unique number value.
+
+  // We begin by expanding our array to include four options.
+  var numberOptions = [10, 5, 3, 7];
+
+
+
+
+
+
+
+for (var i = 0; i < numberOptions.length; i++) {
+
+    // For each iteration, we will create an imageCrystal
+    var imageCrystal = $("<img>");
+
+    // First each crystal will be given the class ".crystal-image".
+    // This will allow the CSS to take effect.
+    imageCrystal.addClass("crystal-image");
+
+    // Each imageCrystal will be given a src link to the crystal image
+    imageCrystal.attr("src", "./assets/images/generic heart-1.png.png"); //figure out how to link ot my images -1/19
+
+    // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // This data attribute will be set equal to the array value.
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+    $("#crystals").append(imageCrystal);
+  }
+
+//this is filler code from w3schools
+
+
+
+document.getElementById("demo").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
+ 
