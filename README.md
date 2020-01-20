8d5090ae8682be9ebc1ecc8e73132bca9b57c76f
Ben Mulholland <ben.mulholland@gmail.com>
2020-01-20 11:03:04 -0500

    cleaning up vars, cleaning up old fragments of abandoned code, trying to test math realize isn't finding javascript file

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Crystal Collector
 a crystal collector game where a random number is generated and your goal is to click on the 'bit gems' to match the number but not go over!
 
 this game uses html, css, and javascript, and initally created on 1/16/20 by Ben Mulholland
+
+gem images and background were created by me in piskellapp
+
+things yet to be added: sound effects for buttons clicked, background sound when overshooting the number, background sound when getting the correct number, 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,90 +1,101 @@
+//scorecard and game variables
 var currentNumber = 0; 
 var win = 0; 
 var lose = 0;
 var randomNumberObject = "";
 
+
+
+function newGame(){
 //Random Number
-var lotteryValue = Math.floor(Math.random() * 120) + 19;
 
-//Random Crystal Value  need to double check
-var crystalValue1 = Math.floor(Math.random() * 12) + 1;
-var crystalValue2 = Math.floor(Math.random() * 12) + 1;
-var crystalValue3 = Math.floor(Math.random() * 12) + 1;
-var crystalValue4 = Math.floor(Math.random() * 12) + 1;
-$(document).ready(function() {
-
-$('currentNumber').val(lotteryValue);
-
-$("button").val(crystalValue1,crystalValue1, crystalValue1,crystalValue1);
-
-score = $('.scoreBox');
-
-        // in case my computer crashes on 1/19/19 go here next https://stackoverflow.com/questions/28103522/how-to-enable-and-disable-onclick-images
-    $(".btn1").click(function () {     
-    }); 
-    $(".btn2").click(function () {                 
-    }); 
-    $(".btn3").click(function () {                 
-    }); 
-    $(".btn4").click(function () {             
-    }); 
-});
+    // are my vars tied to my images? where do they show up -1/20
+    randomNumberObject = Math.floor(Math.random() * 120) + 19;
+        currentNumber = 0;
+        //moving vars into the $doc... to try to clean up what I see and read, i keep losing my place otherwise 1/20
+    var redRandom = Math.floor(Math.random() * 12) + 1;
+    var yellowRandom = Math.floor(Math.random() * 12) + 1;
+    var blueRandom = Math.floor(Math.random() * 12) + 1;
+    var shrekRandom = Math.floor(Math.random() * 12) + 1;
 
 
-$(".btn1").click(function () {     
-    score += crystalValue1;
+                // in case my computer crashes on 1/19/19 go here next https://stackoverflow.com/questions/28103522/how-to-enable-and-disable-onclick-images
+            
+
+
+    console.log("red: " + redRandom);
+    console.log("yellow: " + yellowRandom);
+    console.log("blue: " + blueRandom);
+    console.log("shrek: " + shrekRandom);
+
+     //print score for game
+    document.getElementById("randomNumberObject").innerHTML="Your Number Objective: " + randomNumberObject
+};
+
+
+
+
+
+newGame();
+//what happens when clicked
+
+
+$(".red-gem").on("click",function () {
+    currentNumber += redRandom; saveState()     
 }); 
+
+$(".yellow-gem").click(function () {  
+    currentNumber += yellowRandom; saveState()                
+}); 
+$(".blue-gem").click(function () {  
+    currentNumber += blueRandom; saveState()                
+}); 
+$(".shrek-gem").click(function () {   
+    currentNumber += shrekRandom; saveState()           
+}); 
+
+function saveState(){
+    if (randomNumberObject === currentNumber) {win++;
+        currentNumber += randomNumberObject; alert (Great Job, you did it! Try Again!);
+        document.getElementById("win").innerHTML="Win: " + win;
+        startGame();
+
+    }
+    else if (randomNumberObject <= currentNumber) {lose++;
+        currentNumber += randomNumberObject; alert ("You Lose! Try Again!");
+        document.getElementById("lose").innerHTML="lose: " + lose;
+        startGame();
+
+}
+
+console.log(randomNumberObject)
+
+
+
+
+
+// //this way didn't work keeping untii further troubleshooting
+// (".red-gem").click(function () {
+//     currentNumber += redRandom; saveState()     
+// }); 
+
+// $(".yellow-gem").click(function () {  
+//     currentNumber += yellowRandom; saveState()                
+// }); 
+// $(".blue-gem").click(function () {  
+//     currentNumber += blueRandom; saveState()                
+// }); 
+// $(".shrek-gem").click(function () {   
+//     currentNumber += shrekRandom; saveState()           
+// }); 
+
+// function saveState(){
+//     if (randomNumberObject === currentNumber)
+// }
+
+
 // update display can't find code, will look in homework for how i did it last time 1/19
 
 
 
-
-
-// var targetNumber = 53;  //this will be a random number 
-
-//   $("#number-to-guess").text(targetNumber);
-
-//   var counter = 0;
-
-//   // Now for the hard part. Creating multiple crystals each with their own unique number value.
-
-//   // We begin by expanding our array to include four options.
-//   var numberOptions = [10, 5, 3, 7];
-
-
-
-
-  $buttons()
-
-
-
-for (var i = 0; i < numberOptions.length; i++) {
-
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "./assets/images/generic heart-1.png.png"); //figure out how to link ot my images -1/19
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
-  }
-
-//this is filler code from w3schools
-
-
-
-document.getElementById("demo").onclick = function() {myFunction()};
-
-function myFunction() {
-  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
-}
- 
+//need to add ways to see losses, wins, etc.
